30df6d64e95b370710d9a05d41194a0b445eb07d
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-10 15:16:52 -0700

    fix logic for only 1 initial consonant
    co-authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ function pigLatin(sentence) {
   const firstLetter = sentence.charAt(0);
   const slicedSentence = sentence.slice(1, sentence.length);
   const sentenceArray = sentence.split("");
-  const vowelList = "aeiouAEIOU";
-  console.log(sentenceArray);
-  if (sentenceArray[0] === "a" || sentenceArray[0] === "e" || sentenceArray[0] ===  "i" || sentenceArray[0] ===  "o" || sentenceArray[0] ===  "u"){
+
+  if (sentenceArray[0] === "a" || sentenceArray[0] === "e" || sentenceArray[0] ===  "i" || sentenceArray[0] ===  "o" || sentenceArray[0] ===  "u") {
     return sentence + "way"
-  } else if (sentenceArray[0] !== "a" || sentenceArray[0] === "e" || sentenceArray[0] ===  "i" || sentenceArray[0] ===  "o" || sentenceArray[0] ===  "u" && 
-            sentenceArray[1] !== "a" || sentenceArray[0] === "e" || sentenceArray[0] ===  "i" || sentenceArray[0] ===  "o" || sentenceArray[0] ===  "u") {
-    return  sentenceArray.join("").slice(2) + sentenceArray[0] + sentenceArray[1] + "ay";
-  } else {return slicedSentence + sentenceArray[0] + "ay"}
+  } else if (sentenceArray[0] !== "a" || sentenceArray[0] !== "e" || sentenceArray[0] !==  "i" || sentenceArray[0] !==  "o" || sentenceArray[0] !==  "u" && 
+            sentenceArray[1] === "a" || sentenceArray[1] === "e" || sentenceArray[1] ===  "i" || sentenceArray[1] ===  "o" || sentenceArray[1] ===  "u") {
+    return  sentenceArray.join("").slice(1) + sentenceArray[0] + "ay";
+  } 
+  else {return slicedSentence + sentenceArray[0] + sentenceArray[1] + "ay"}
 };
 
 
@@ -22,8 +22,6 @@ $(document).ready(function() {
     const sentence = $("input#sentence").val();
     const result = pigLatin(sentence);
     $("#result").text(result);
-  //   if (sentence.charAt(0) === "a" || sentence.charAt(0) === "e" ||sentence.charAt(0) ===  "i" ||sentence.charAt(0) ===  "o" ||sentence.charAt(0) ===  "u"){return sentence + "way"
-  // }else {return sentence}
   console.log()
   });
 });
